0db5329153a7b3771c77dab429fffe0195e04c9b
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-04-18 20:16:17 +0200

    Initial Commit

---
+++ CalculatorInSwiftUI/CalculatorInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorInSwiftUIApp.swift
+//  CalculatorInSwiftUI
+//
+//  Created by Stjepan Stojčević on 18.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct CalculatorInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
